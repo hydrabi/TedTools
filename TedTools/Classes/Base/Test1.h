56eed253ea010023e4a41993f3716c688a3479e4
hydrabi <bizhifengted@gmail.com>
2019-05-20 13:15:31 +0800

    no message

--- TedTools/Classes/Base/Test1.h
+++ TedTools/Classes/Base/Test1.h
@@ -1,5 +1,5 @@
 //
-//  TEST1.h
+//  Test1.h
 //  Pods-TedTools_Example
 //
 //  Created by iMac on 2019/5/20.
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TEST1 : NSObject
+@interface Test1 : NSObject
 
 @end
 
