c4130331d2a660c590cd69d67a5d3f383bfe3f33
hydrabi <bizhifengted@gmail.com>
2019-05-20 12:20:31 +0800

    测试

---
+++ TedTools/Classes/Base/TEST1.h
@@ -0,0 +1,16 @@
+//
+//  TEST1.h
+//  Pods-TedTools_Example
+//
+//  Created by iMac on 2019/5/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TEST1 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TedTools/Classes/Base/TEST1.m
@@ -0,0 +1,12 @@
+//
+//  TEST1.m
+//  Pods-TedTools_Example
+//
+//  Created by iMac on 2019/5/20.
+//
+
+#import "TEST1.h"
+
+@implementation TEST1
+
+@end
