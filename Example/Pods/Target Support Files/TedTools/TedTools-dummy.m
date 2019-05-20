5df02f5c7f4bba2de73178d366f1a6f7c8b00ee9
hydrabi <bizhifengted@gmail.com>
2019-05-20 10:28:47 +0800

    init library

---
+++ Example/Pods/Target Support Files/TedTools/TedTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TedTools : NSObject
+@end
+@implementation PodsDummy_TedTools
+@end
---
+++ TedTools/Classes/TestObj.m
@@ -0,0 +1,17 @@
+//
+//  TestObj.m
+//  TedTools_Example
+//
+//  Created by iMac on 2019/5/20.
+//  Copyright © 2019 1016590797@qq.com. All rights reserved.
+//
+
+#import "TestObj.h"
+
+@implementation TestObj
+
+-(void)say{
+    NSLog(@"Hello");
+}
+
+@end
